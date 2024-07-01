20325a4cbff780518d59ea9ba348fdc480035cff
patilmayur-02 <patilmayur2960@gmail.com>
2024-07-01 11:33:41 +0530

    Add files via upload

---
+++ pr.1.1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{ 
+   int degreesCelsius , Fahrenheit;
+   int f= (9/5 *°C) + 32;
+   printf("enter the degreesCelsius : ");
+    scanf =("%F",&degreesCelsius );
+  
+   printf("entey the Fahrenheit = %f.3f",f ");
+
+}
---
+++ pr.1.2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+ 
+ main()
+ 
+ {
+ 	int basesalary,grosssalary ,HRA,DA,TA ;
+ 	int Basesalary=100;
+ 	HRA=10;
+ 	DA=5;
+ 	TA=8;
+ 	printf ("enter your base salary");
+ 	sacnf ("%d,&base salary");
+ 	
+ 	printf("Gross salary = %d", basesalary*10/100);
