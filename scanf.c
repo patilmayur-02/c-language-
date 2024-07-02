0e829fd8e1e24ffe072b665589001e4c84731cca
patilmayur-02 <patilmayur2960@gmail.com>
2024-07-02 12:42:33 +0530

    Add files via upload

---
+++ scanf.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+
+{  
+   int x;
+   int y;
+   
+   
+   printf("enter the vale of x :");
+   scanf("%d",& x);
+   printf("enter the vale of y :");
+   scanf("%d",& y);
+   
+   (x<y)?printf ("x is min"):printf("y is min");
+   
+}
