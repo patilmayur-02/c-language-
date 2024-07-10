dfb2fea3cc866e6f6f2f11f5fa65ee21a411b4fb
Mayur <patilmayur2960@gmail.com>
2024-07-10 11:08:44 +0530

    Add files via upload

---
+++ ternary/if else .c.3.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+main ()
+
+{
+  int english, hindi, gujrati;
+  printf("enter the marks of\nenglish:\n hindi:\n gujrati:\n ");
+  scanf("%d%d%d",english, hindi, gujrati);
+  
+}
