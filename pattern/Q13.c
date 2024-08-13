7862747250e338301ff4710002b5ff191a02e14e
Mayur <patilmayur2960@gmail.com>
2024-08-13 10:04:02 +0530

    Add files via upload

---
+++ pattern/Q13.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+void main()
+{
+  char i,j,k='A';
+  
+  for(i='A';i<='E';i++)
+  {
+		for(j='A';j<=i;j++)
+	 	 {
+	  		printf("%c ",k);
+	  		k++;
+	 	 }
+	 	 printf("\n");
+  }
+
+}
