1f56d5ad5c68132b9df2a59507848425d33c4ac9
Mayur <patilmayur2960@gmail.com>
2024-09-21 09:58:58 +0530

    Add files via upload

---
+++ C leng final exam/Q3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+
+{
+	 int a,b;
+ 
+ 	printf("enter the size of array");
+ 	scanf("%d"&a);
+ 	
+ 	
+
+	
+}
---
+++ C leng final exam/q5.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+{
+	float  fahrenheit,degree , celsius ;
+	printf("enter celsius :");
+	scanf("%f,& celsius");
+	
+    printf("fahrenheit=°F = (9/5 * °C) + 32");
+
+	printf("fahrenheit :%f%f,fahrenheit");
+																	
+																	
+}
